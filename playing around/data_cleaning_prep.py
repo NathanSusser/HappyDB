2234129b6ea90788bc4149c1a3d41ac1f2ba26f8
NathanSusser <nathansusser@gmail.com>
2024-10-29 01:53:38 -0400

    PERMA data cleaning and prep
    Preping for openai test run hopefully tomorrow

--- playing around/data_cleaning_prep.py
+++ playing around/data_cleaning_prep.py
@@ -11,6 +11,9 @@ hbd_df = pd.read_csv('happydb/data/cleaned_hm.csv')
 #read in PERMA items and dimensions
 PERMA_df = pd.read_csv('Profiles/PERMA.csv')
 
+#read in PERMA_subject items and dimensions
+PERMA_sub_df = pd.read_csv('Profiles/PERMA-SUBJECT.csv')
+
 #read in the WB-PRo items and dimensions
 WBP_df = pd.read_csv('Profiles/WB-Pro.csv')
 
@@ -23,19 +26,32 @@ WBP_df = pd.read_csv('Profiles/WB-Pro.csv')
 #read in the PANAS items and dimensions
 #PANAS_df = pd.read_csv('/Profiles/PANAS.csv')
 
-#pull the clean sentences from the happyDB data
-clean_sentences = hbd_df['cleaned_hm']
+# Pull the clean sentences from the happyDB data
+clean_sentences = hbd_df[['hmid', 'reflection_period', 'cleaned_hm']]
 print(clean_sentences.head())
 
-# Extract the second column (Items) from PERMA_df and WBP_df
-PERMA_items = PERMA_df.iloc[:, 1]  # Second column
-WBP_items = WBP_df.iloc[:, 1]      # Second column
+# Get distinct items in 'reflection_period'
+distinct_reflection_periods = clean_sentences['reflection_period'].unique()
+print(distinct_reflection_periods)
 
-# Combine items from both dataframes to create the list of column names
-column_names = list(PERMA_items) + list(WBP_items)
+# Set 'hmid', 'reflection_period', and 'cleaned_hm' as multi-index
+clean_sentences.set_index(['hmid', 'reflection_period', 'cleaned_hm'], inplace=True)
 
-# Create the matrix with clean sentences as rows and combined items as columns
-matrix = pd.DataFrame(0, index=clean_sentences, columns=column_names)
+# Extract the second column (Items) from PERMA_df
+#PERMA_items = PERMA_df.iloc[:, 1]  
 
+# Extract the second column (Items) from PERMA_df
+#WBP_items = WBP_df.iloc[:, 1]  
+
+# Extract the second column (Items) from PERMA_df
+PERMA_sub_items = PERMA_sub_df.iloc[:, 1]  
+
+# Combine items from PERMA_df to create the list of column names
+column_names = list(PERMA_sub_items)  # + list(WBP_items)
+
+# Create the matrix with 'cleaned_hm' as rows and combined items as columns
+matrix = pd.DataFrame(0, index=clean_sentences.index, columns=column_names)
+
+print(matrix.columns[1:])
 #download the matrix
 matrix.to_csv('/Users/nsusser/Desktop/Github/happyDB/playing around/my data/clean_open_ai.csv', index=True)
